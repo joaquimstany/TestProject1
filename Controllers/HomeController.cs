04495cdc2caba371eaf829eb52a322b46d8d9cbf
joaquimstany <joaquimstany@gmai.com>
2020-04-07 22:35:17 +0530

    rror change Porject

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,7 +7,24 @@ using Microsoft.AspNetCore.Mvc;
 using TestProject1.Models;
 
 namespace TestProject1.Controllers
-{"] = "Your contact page.";
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
 
             return View();
         }
